7511fa0817633b0e68cf03895399004bf8ebe1c6
alvarorubioc <alvaro.rubiocamino@gmail.com>
2026-06-09 17:17:24 +0200

    Change logos soluciones to marquee

--- template-parts/logos-clientes-soluciones.php
+++ template-parts/logos-clientes-soluciones.php
@@ -56,64 +56,61 @@ if ( ! empty( $sector_term_ids ) ) {
 }
 
 $clientes_query = new WP_Query( $clientes_query_args );
+
+$render_logo_marquee_items = static function () use ( $clientes_query ) {
+	while ( $clientes_query->have_posts() ) :
+		$clientes_query->the_post();
+		$cliente_link = '';
+
+		if ( function_exists( 'get_field' ) ) {
+			$cliente_link = get_field( 'link_cliente' );
+		}
+
+		if ( empty( $cliente_link ) ) {
+			$cliente_link = get_post_meta( get_the_ID(), 'link_cliente', true );
+		}
+
+		if ( ! has_post_thumbnail() ) {
+			continue;
+		}
+		?>
+		<figure class="smn-clientes-logos__item cliente-<?php the_ID(); ?>">
+			<?php if ( ! empty( $cliente_link ) ) : ?>
+				<a href="<?php echo esc_url( $cliente_link ); ?>" target="_blank" rel="nofollow noopener">
+			<?php endif; ?>
+
+			<?php
+			echo get_the_post_thumbnail(
+				get_the_ID(),
+				'full',
+				array(
+					'class'    => 'smn-clientes-logos__img',
+					'loading'  => 'lazy',
+					'decoding' => 'async',
+				)
+			);
+			?>
+
+			<?php if ( ! empty( $cliente_link ) ) : ?>
+				</a>
+			<?php endif; ?>
+		</figure>
+		<?php
+	endwhile;
+};
 ?>
 
 <div class="smn-clientes-shortcode">
-	<?php if ( $clientes_query->have_posts() ) : ?>
-		<div class="smn-clientes-logos facetwp-template">
-			<?php
-			while ( $clientes_query->have_posts() ) :
-				$clientes_query->the_post();
-				$cliente_link = '';
-
-				if ( function_exists( 'get_field' ) ) {
-					$cliente_link = get_field( 'link_cliente' );
-				}
-
-				if ( empty( $cliente_link ) ) {
-					$cliente_link = get_post_meta( get_the_ID(), 'link_cliente', true );
-				}
-
-				if ( ! has_post_thumbnail() ) {
-					continue;
-				}
-				?>
-				<div class="smn-clientes-logos__item cliente-<?php the_ID(); ?>">
-					<?php if ( ! empty( $cliente_link ) ) : ?>
-						<a href="<?php echo esc_url( $cliente_link ); ?>" target="_blank" rel="nofollow noopener">
-					<?php endif; ?>
-
-					<?php
-					echo get_the_post_thumbnail(
-						get_the_ID(),
-						'full',
-						array(
-							'class'    => 'smn-clientes-logos__img',
-							'loading'  => 'lazy',
-							'decoding' => 'async',
-						)
-					);
-					?>
-
-					<?php if ( ! empty( $cliente_link ) ) : ?>
-						</a>
-					<?php endif; ?>
-				</div>
-			<?php endwhile; ?>
-		</div>
-		<?php wp_reset_postdata(); ?>
-        <div class="wp-block-buttons is-layout-flex wp-block-buttons-is-layout-flex smn-clientes-logos__actions">
-			<div class="wp-block-button">
-				<button
-					type="button"
-					class="wp-block-button__link wp-element-button smn-clientes-logos__toggle"
-					data-label-more="<?php echo esc_attr__( 'Mostrar todos', 'infinitia' ); ?>"
-					data-label-less="<?php echo esc_attr__( 'Mostrar menos', 'infinitia' ); ?>"
-					aria-expanded="false"
-				>
-					<?php echo esc_html__( 'Mostrar todos', 'infinitia' ); ?>
-				</button>
+	<div class="infinite-scroll is-faded is-slower is-layout-flex is-nowrap">
+		<?php if ( $clientes_query->have_posts() ) : ?>
+			<div class="wp-block-group is-nowrap is-layout-flex">
+				<?php $render_logo_marquee_items(); ?>
 			</div>
-		</div>
-	<?php endif; ?>
+			<?php $clientes_query->rewind_posts(); ?>
+			<div class="wp-block-group is-nowrap is-layout-flex" aria-hidden="true">
+				<?php $render_logo_marquee_items(); ?>
+			</div>
+			<?php wp_reset_postdata(); ?>
+		<?php endif; ?>
+	</div>
 </div>
