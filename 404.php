835a9a1abce362d15b3c0c5062eac5796735cdda
alvarorubioc <alvaro.rubiocamino@gmail.com>
2026-07-22 20:08:33 +0200

    404 page

--- 404.php
+++ 404.php
@@ -12,44 +12,28 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'infinitia' ); ?></h1>
-			</header><!-- .page-header -->
+		<section class="error-404 not-found entry-content wp-block-post-content has-global-padding is-layout-constrained wp-block-post-content-is-layout-constrained">
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'infinitia' ); ?></p>
+			<div class="page-content wp-block-group is-style-margin-vertical">
+				<div class="wp-block-group is-style-margin-vertical--top section-caption is-layout-flex wp-container-core-group-is-layout-13c19379 wp-block-group-is-layout-flex">
+					<div class="wp-block-safe-svg-svg-icon safe-svg-cover" style="text-align: left;">
+						<div class="safe-svg-inside safe-svg-inline" style="width: 16px; height: 16px; background-color: var(--wp--preset--color--); color: var(--wp--preset--color--);">
+							<!--?xml version="1.0" encoding="UTF-8"?-->
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="8" viewBox="0 0 16 8" fill="none" id="caption-title-icon">
+								<path d="M4.00029 8C6.20958 8 8.00057 6.20914 8.00057 4C8.00057 1.79086 6.20958 0 4.00029 0C1.79099 0 0 1.79086 0 4C0 6.20914 1.79099 8 4.00029 8Z" fill="#088988"></path>
+								<path d="M11.9997 8C14.209 8 16 6.20914 16 4C16 1.79086 14.209 0 11.9997 0C9.79042 0 7.99943 1.79086 7.99943 4C7.99943 6.20914 9.79042 8 11.9997 8Z" fill="#088988"></path>
+							</svg>
+						</div>
+					</div>
+					<div class="wp-block-smn-parent-title" id="parent-title-quienes-somos">404</div>
+				</div>
+
+				<h1 class="page-title"><?php esc_html_e( 'Oops! No podemos encontrar lo que estás buscando', 'infinitia' ); ?></h1>
+				<p><?php esc_html_e( 'Parece que no se ha encontrado nada en esta página. ¿Por qué no pruebas a ir a la página de inicio o a hacer una búsqueda?', 'infinitia' ); ?></p>
 
 					<?php
 					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'infinitia' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$smn_hybrid_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'infinitia' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$smn_hybrid_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+					;?>
 
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
