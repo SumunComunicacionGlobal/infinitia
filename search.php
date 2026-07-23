9703da955e2ebc3dd5ce5bd8137c547cb8b5d8f2
alvarorubioc <alvaro.rubiocamino@gmail.com>
2026-07-23 12:34:41 +0200

    Fix popover scroll

--- search.php
+++ search.php
@@ -10,44 +10,35 @@
 get_header();
 ?>
 
+
 	<main id="primary" class="site-main">
 
-		<?php if ( have_posts() ) : ?>
+	 	<?php smn_render_reusable_block_by_title( 'Hero Archive' ); ?>
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'infinitia' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
-			</header><!-- .page-header -->
+		<div class="entry-content wp-block-post-content has-global-padding is-layout-constrained">
+			<div class="wp-block-group is-style-margin-vertical">
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
 
-			endwhile;
+				<?php if ( have_posts() ) : ?>
 
-			the_posts_navigation();
+					<?php smn_render_reusable_block_by_title( 'Loop Default' );
 
-		else :
+					the_posts_navigation();
 
-			get_template_part( 'template-parts/content', 'none' );
+				else :
 
-		endif;
-		?>
+					get_template_part( 'template-parts/content', 'none' );
+
+				endif;
+				?>
+			</div>
+		</div><!-- .entry-content -->
 
 	</main><!-- #main -->
 
+	
+
 <?php
-get_sidebar();
 get_footer();
